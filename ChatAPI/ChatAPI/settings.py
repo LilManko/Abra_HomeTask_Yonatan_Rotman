76bbc720a719ec6822e059bd8f44f33a7030b74a
YonatanRotman <yonirotman@gmail.com>
2022-11-17 03:35:36 +0200

    reverted settings

--- ChatAPI/ChatAPI/settings.py
+++ ChatAPI/ChatAPI/settings.py
@@ -46,8 +46,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     "corsheaders",
     'base',
-    "whitenoise.runserver_nostatic",
-    "django.contrib.staticfiles",
+    
 ]
 
 MIDDLEWARE = [
